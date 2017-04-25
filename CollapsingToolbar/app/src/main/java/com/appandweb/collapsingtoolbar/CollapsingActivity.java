974f177cccacaf948311e88234a1edccfa10cdcd
Jose Armando <armando@appandweb.es>
2017-04-25 12:39:46 +0200

    CollapsingToolbar added

---
+++ CollapsingToolbar/app/src/main/java/com/appandweb/collapsingtoolbar/CollapsingActivity.java
@@ -0,0 +1,34 @@
+package com.appandweb.collapsingtoolbar;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v4.content.ContextCompat;
+
+public class CollapsingActivity extends BaseActivity {
+    CollapsingToolbarLayout ctb;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ctb = (CollapsingToolbarLayout) findViewById(R.id.collapsing_ctb_toolbar);
+        configurateToolbar();
+    }
+
+    private void configurateToolbar() {
+        ctb.setCollapsedTitleTextColor(Color.WHITE);
+        ctb.setContentScrimColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        ctb.setExpandedTitleColor(ContextCompat.getColor(this, android.R.color.transparent));
+        ctb.setTitle(this.getResources().getString(R.string.app_name));
+    }
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.activity_collpasing;
+    }
+
+    @Override
+    public boolean hideToolbar() {
+        return true;
+    }
+}
